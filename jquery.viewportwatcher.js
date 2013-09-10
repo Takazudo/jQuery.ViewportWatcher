545687a5afd584d213f256f58f0cef5af1c8cdeb
Takeshi Takatsudo <takazudo@gmail.com>
2013-09-11 01:14:59 +0900

    bumped version number to v0.1.0

--- jquery.viewportwatcher.js
+++ jquery.viewportwatcher.js
@@ -1,6 +1,6 @@
 /*! jQuery.viewportWatcher (https://github.com/Takazudo/jQuery.viewportWatcher)
  * lastupdate: 2013-09-11
- * version: 0.0.0
+ * version: 0.1.0
  * author: 'Takazudo' Takeshi Takatsudo <takazudo@gmail.com>
  * License: MIT */
 (function() {
